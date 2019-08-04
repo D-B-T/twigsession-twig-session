5daa5918ccfbce816e88e815a5219810c15e3b1d
Deep Blue <matt@e-finityleads.co.uk>
2019-08-04 19:36:52 +0100

    Add files via upload

---
+++ src/translations/en/twig-session.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Twig Session plugin for Craft CMS 3.x
+ *
+ * A session plugin for twig.
+ *
+ * @link      candid.co.uk
+ * @copyright Copyright (c) 2019 Joe The Boss
+ */
+
+/**
+ * Twig Session en Translation
+ *
+ * Returns an array with the string to be translated (as passed to `Craft::t('twig-session', '...')`) as
+ * the key, and the translation as the value.
+ *
+ * http://www.yiiframework.com/doc-2.0/guide-tutorial-i18n.html
+ *
+ * @author    Joe The Boss
+ * @package   TwigSession
+ * @since     1.0.0
+ */
+return [
+    'Twig Session plugin loaded' => 'Twig Session plugin loaded',
+];
